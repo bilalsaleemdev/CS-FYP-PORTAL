cd4098b2b5da1355bdf32ef9d37dc9fe5bb5e88e
bilalsaleemdev <bilalsaleem.dev@gmail.com>
2021-09-16 12:35:44 +0500

    Changes

--- src/MainPage/Main/Dashboard/index.jsx
+++ src/MainPage/Main/Dashboard/index.jsx
@@ -11,16 +11,13 @@ import ApproveRequest from '../../HR/Sales/approverequest';
 import ApproveRequest1 from '../../HR/Sales/approverequest1';
 
 
-
 const DashboardRoute = ({ match }) => (
    <Switch>
       <Redirect exact from={`${match.url}/`} to={`${match.url}/dashboard`} />
       <Route path={`${match.url}/dashboard/manager`} component={ManagerDashboard} />
       <Route path={`${match.url}/dashboard/employee`} component={EmployeeDashboard} />
       <Route path={`${match.url}/dashboard/ceo`} component={CeoDashboard} />
-      <Route path={`${match.url}/approve-request`} component={ApproveRequest} />
-      <Route path={`${match.url}/approve-request1`} component={ApproveRequest1} />
-
+      <Route path={`${match.url}/approve-request`} component={ApproveRequest} />      
    </Switch>
   
 );
