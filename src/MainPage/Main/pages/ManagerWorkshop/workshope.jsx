80f272368b9262e2ac324a06c6170c2c69f9ddf9
bilalsaleemdev <bilalsaleem.dev@gmail.com>
2021-09-27 15:52:47 +0500

    Changes

--- src/MainPage/Main/pages/ManagerWorkshop/workshope.jsx
+++ src/MainPage/Main/pages/ManagerWorkshop/workshope.jsx
@@ -14,6 +14,15 @@ const Workshopes = () => {
   const cancelTokenSource = axios.CancelToken.source();
   var user_id = localStorage.getItem('user_id');
 
+  var workShpoeEdit = {
+    id: 0,
+    purpose: '',
+    day: '',
+    last_at: '',
+    start_at: '',
+    url: '',
+
+  }
   const [purpose, setPurpose] = useState();
   const [day, setDay] = useState();
   const [start_at, setStart_at] = useState('2021-01-01');
@@ -22,43 +31,51 @@ const Workshopes = () => {
   const [conferanceData, setConferanceData] = useState([]);
 
   // let conferanceData = [1,2];
-  useEffect(() => {    
+  useEffect(() => {
     getUserWorkshopes()
   }, [])
 
   $('#empid').on('change', function (e) {
-    setDay(e.target.value)    
+    setDay(e.target.value)
   });
-  
-  const createWorkShope = async() => {
-    const data = { 'day': day , 'url':url,'start_at':start_at, 'last_at':last_at ,'purpose': purpose};
-    const response = await createWorkShopeAPI(data,cancelTokenSource.token); 
-    if(response.success == true){
-      console.log('Data::',response.data.id)
-      createUserWorkShope(localStorage.getItem('user_id'),response.data.id)
+
+  const createWorkShope = async () => {
+    const data = { 'day': parseInt(day) , 'url': url, 'start_at': start_at, 'last_at': last_at, 'purpose': purpose };
+    const response = await createWorkShopeAPI(data, cancelTokenSource.token);
+    if (response.success == true) {
+      console.log('Data::', response.data.id)
+      createUserWorkShope(localStorage.getItem('user_id'), response.data.id)
     }
   };
 
-  const createUserWorkShope = async(user_id,workshope_id) => {
-    const data = { 'user_id': user_id , 'conference_id':workshope_id};
-    const response = await createUserWorkShopeAPI(data,cancelTokenSource.token); 
-    if(response.success == true){
+  const createUserWorkShope = async (user_id, workshope_id) => {
+    const data = { 'user_id': user_id, 'conference_id': workshope_id };
+    const response = await createUserWorkShopeAPI(data, cancelTokenSource.token);
+    if (response.success == true) {
       getUserWorkshopes()
     }
   };
 
-  const getUserWorkshopes = async() => {    
-    const response = await getUserWorkshopesAPI( 
-      user_id,    
+  const getUserWorkshopes = async () => {
+    const response = await getUserWorkshopesAPI(
+      user_id,
       cancelTokenSource.token
-    );    
-    if(response.success == true){      
-      setConferanceData([...[],...response.data])
+    );
+    if (response.success == true) {
+      setConferanceData([...[], ...response.data])
     }
   };
-  
-  
-  
+  const editWorkshope = (item) => {
+    setPurpose(item.purpose)
+    setDay(toString(item.day))
+    setLast_at(item.last_at)
+    setStart_at(item.start_at)
+    setUrl(item.url)
+    console.log('workShpoeEdit:', workShpoeEdit.day)
+  }
+
+
+
   return (
     <>
       <div className="page-wrapper">
@@ -79,44 +96,45 @@ const Workshopes = () => {
                 </ul>
               </div>
               <div className="col-auto float-right ml-auto">
-                <a href="#" className="btn add-btn" data-toggle="modal" data-target="#create_project"><i className="fa fa-plus" /> Create Workshope</a>                
+                <a href="#" className="btn add-btn" data-toggle="modal" data-target="#create_project"><i className="fa fa-plus" /> Create Workshope</a>
               </div>
             </div>
           </div>
 
           <div className="row">
-            { 
-              conferanceData.map((key,item) => (
+            {
+              conferanceData.map((key, item) => (
                 <div className="col-lg-4 col-sm-6 col-md-4 col-xl-3">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="dropdown dropdown-action profile-action">
-                      <a href="#" className="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i className="material-icons">more_vert</i></a>
-                      <div className="dropdown-menu dropdown-menu-right">
-                        <a className="dropdown-item" href="#" data-toggle="modal" data-target="#edit_project"><i className="fa fa-pencil m-r-5" /> Edit</a>
-                        <a className="dropdown-item" href="#" data-toggle="modal" data-target="#delete_project"><i className="fa fa-trash-o m-r-5" /> Delete</a>
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="dropdown dropdown-action profile-action">
+                        <div className="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i className="material-icons">more_vert</i></div>
+                        <div className="dropdown-menu dropdown-menu-right">
+                          {/* data-toggle="modal" */}                          
+                          <a className="dropdown-item" onClick={() => editWorkshope(key)} data-toggle="modal" data-target="#create_project"><i className="fa fa-pencil m-r-5" /> Edit</a>
+                          <a className="dropdown-item" href="#" data-toggle="modal" data-target="#delete_project"><i className="fa fa-trash-o m-r-5" /> Delete</a>
+                        </div>
+                      </div>
+                      <h4 className="project-title"><a href="/app/projects/projects-view">Manager WorkShope</a></h4>
+                      <p className="text-muted">{key.purpose}
+                      </p>
+                      <div className="pro-deadline m-b-15">
+                        <div className="sub-title">
+                          day:
+                    </div>
+                        <div className="text-muted">
+                          {key.day}
+                        </div>
+                      </div>
+                      <div className="project-members m-b-15">
+                        <div>Start At:<span>{key.start_at}</span></div>
+                        <div>Start At:<span>{key.last_at}</span></div>
                       </div>
                     </div>
-              <h4 className="project-title"><a href="/app/projects/projects-view">Management WorkShope</a></h4>                    
-                    <p className="text-muted">{key.purpose}
-                  </p>
-                    <div className="pro-deadline m-b-15">
-                      <div className="sub-title">
-                        day:
-                    </div>
-                      <div className="text-muted">
-                      {key.day}
-                    </div>
-                    </div>
-                    <div className="project-members m-b-15">
-                      <div>Start At:<span>{key.start_at}</span></div>
-                      <div>Start At:<span>{key.last_at}</span></div>                      
-                    </div>                    
                   </div>
                 </div>
-              </div>
-              ))              
-            }            
+              ))
+            }
           </div>
         </div>
         {/* /Page Content */}
@@ -169,11 +187,11 @@ const Workshopes = () => {
                       <div className="form-group">
                         <label>Start Datesss</label>
                         <div id="stdid" className="cal-icon">
-                          <input                            
+                          <input
                             className="form-control datetimepicker"
                             type="text"
                             value={start_at}
-                            onChange={(e)=>setStart_at(e.target.value)}
+                            onChange={(e) => setStart_at(e.target.value)}
                           />
                         </div>
                       </div>
@@ -184,12 +202,12 @@ const Workshopes = () => {
                         <div className="cal-icon">
                           <input
                             value={last_at}
-                            onChange={(e)=>setLast_at(e.target.value)}
+                            onChange={(e) => setLast_at(e.target.value)}
                             className="form-control datetimepicker" type="text" />
                         </div>
                       </div>
                     </div>
-                  </div>                
+                  </div>
                   <div className="row">
                     <div className="col-sm-6">
                       <div className="form-group">
@@ -199,11 +217,11 @@ const Workshopes = () => {
                           onChange={(e) => setUrl(e.target.value)}
                           className="form-control" type="text" />
                       </div>
-                    </div>                    
+                    </div>
                   </div>
                   <div className="submit-section">
-                    <div onClick={createWorkShope}className="btn btn-primary">Submit</div>
-                  </div>                  
+                    <div onClick={()=>createWorkShope()} className="btn btn-primary">Submit</div>
+                  </div>
                 </form>
               </div>
             </div>
@@ -215,7 +233,7 @@ const Workshopes = () => {
           <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title">Edit Project</h5>
+                <h5 className="modal-title">Edit Workshope</h5>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">×</span>
                 </button>
@@ -226,7 +244,7 @@ const Workshopes = () => {
                     <div className="col-sm-6">
                       <div className="form-group">
                         <label>Project Name</label>
-                        <input className="form-control" defaultValue="Project Management" type="text" />
+                        <input value={workShpoeEdit.purpose} className="form-control" defaultValue="Project Management" type="text" />
                       </div>
                     </div>
                     <div className="col-sm-6">
