ebec350ff608f07df75571f005d46cd9ec675895
hafizawais154 <awaisyasin154@gmail.com>
2022-01-16 23:33:26 +0500

    deleted some files

--- src/initialpage/App.jsx
+++ src/initialpage/App.jsx
@@ -4,21 +4,14 @@ import { Redirect, Route, Switch } from "react-router-dom";
 //Authendication
 import LoginPage from "./loginpage";
 import RegistrationPage from "./RegistrationPage";
-import ForgotPassword from "./forgotpassword";
-import OTP from "./otp";
-import LockScreen from "./lockscreen";
+
 import { Dashboard } from "./Dashboard/dashboard";
 
-import ApplyJobs from "./ApplyJob";
 
 //Main App
 import DefaultLayout from "./Sidebar/DefaultLayout";
-import Settinglayout from "./Sidebar/Settinglayout";
-import Tasklayout from "./Sidebar/tasklayout";
-import Emaillayout from "./Sidebar/emaillayout";
-import chatlayout from "./Sidebar/chatlayout";
 
-import uicomponents from "../MainPage/UIinterface/components";
+
 //Error Page
 import Error404 from "../MainPage/Pages/ErrorPage/error404";
 import Error500 from "../MainPage/Pages/ErrorPage/error500";
@@ -26,25 +19,7 @@ import Error500 from "../MainPage/Pages/ErrorPage/error500";
 // import 'Assets/css/font-awesome.min.css';
 
 import $ from "jquery";
-// window.jQuery = $;
-// window.$ = $;
-// import UserPage from './pages/UserPage'
-/**
- * Initial Path To Check Whether User Is Logged In Or Not
- */
-// const InitialPath = ({ component: Component, ...rest, authUser }) =>
-//    <Route
-//       {...rest}
-//       render={props =>
-//          authUser
-//             ? <Component {...props} />
-//             : <Redirect
-//                to={{
-//                   pathname: '/login',
-//                   state: { from: props.location }
-//                }}
-//             />}
-//    />;
+
 
 export default class App extends Component {
   componentDidMount() {
@@ -87,19 +62,15 @@ export default class App extends Component {
         <Route exact path="/app/main/dashboard" component={Dashboard} />
         <Route path="/login" component={LoginPage} />
 
-        <Route path="/forgotpassword" component={ForgotPassword} />
+     
         <Route path="/register" component={RegistrationPage} />
-        <Route path="/otp" component={OTP} />
-        <Route path="/lockscreen" component={LockScreen} />
-        <Route path="/applyjob" component={ApplyJobs} />
+    
+        
 
         <Route path="/app" component={DefaultLayout} />
-        <Route path="/settings" component={Settinglayout} />
-        <Route path="/tasks" component={Tasklayout} />
-        <Route path="/email" component={Emaillayout} />
-        <Route path="/conversation" component={chatlayout} />
+  
 
-        <Route path="/ui-components" component={uicomponents} />
+        
         <Route path="/error-404" component={Error404} />
         <Route path="/error-500" component={Error500} />
       </Switch>
